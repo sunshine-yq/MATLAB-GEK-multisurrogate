a6c3ffefa8e8512f4d87880a1811061559ae06ae
amirkb91 <amirkbagheri@gmail.com>
2020-06-11 17:26:12 +0100

    Calculated all theta01's for all models

--- main.m
+++ main.m
@@ -11,15 +11,15 @@ clear; close all; addpath(genpath('./'));
 %% Set Options for running the code
 
 options.nsurrogates  = 10;
-options.activesrrgt  = 1;
+options.activesrrgt  = 10;
 
 options.platform     = 'local';
-options.objective    = 'iterate';
+options.objective    = 'verify';
 
 options.nfiles       = 1;
 options.npredpoints  = 1000;
 options.nnextsamples = 50;
-options.theta        = '';
+options.theta        = 'theta01';
 
 options.writetofile  = false;
 
